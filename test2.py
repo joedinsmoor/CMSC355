680d222f8e6de8e66027cfce0c8cd2517059e000
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-10-23 14:36:29 -0400

    fix test2

--- test2.py
+++ test2.py
@@ -3,8 +3,7 @@ import unittest
 
 class TestSum(unittest.TestCase):
     def test_char(self):
-        with self.assertRaises(TypeError):
-            funcs.joeaddition("a", "b")
+        self.assertNotEqual(funcs.joeaddition(1,2), 4)
 
 if __name__ == '__main__':
     unittest.main()
